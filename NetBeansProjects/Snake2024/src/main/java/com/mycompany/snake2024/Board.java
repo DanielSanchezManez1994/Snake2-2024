22847fb9079cea9bac6c1b50e935b886516ec97e
danielsanchez <dansanman@alu.edu.gva.es>
2024-05-28 20:14:07 +0200

    Ver 1.0.0

--- NetBeansProjects/Snake2024/src/main/java/com/mycompany/snake2024/Board.java
+++ NetBeansProjects/Snake2024/src/main/java/com/mycompany/snake2024/Board.java
@@ -194,7 +194,7 @@ public class Board extends javax.swing.JPanel {
         if(iaTimer != null && timer.isRunning()){
             timer.stop();
         }
-        iaTimer = new Timer((int) (ConfigData.getInstance().getDeltaTime()*1.3), new ActionListener() {
+        iaTimer = new Timer((int) (ConfigData.getInstance().getDeltaTime()*1.4), new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent ae) {
